5fc278da211f6577df9f8f63a47cc0e0b069d8a4
Roman <roman.abrosimov@flant.com>
2017-08-11 10:28:41 +0300

    fix some warnings

--- p2p-node.go
+++ p2p-node.go
@@ -2,47 +2,50 @@ package main
 
 import (
 	"encoding/json"
-	"strings"
 	"strconv"
-//	"time"
+	"strings"
+	//	"time"
 	"flag"
-	"net"
 	"fmt"
+	"net"
 	"os"
 )
 
+// Peers : Карта содержащая список пиров (дописать инфу о частях)
 type Peers map[string]int
 
+// Parts : Карта сожержащая начало области данных и номер части
 type Parts map[int]int64
 
+// Mssg : Структура для общения между нодами (сериализуется в json)
 type Mssg struct {
-	Header	string	`json:"header"`
-	Part	int	`json:"part"`
-	Data	[]byte	`json:"data"`
+	Header string `json:"header"`
+	Part   int    `json:"part"`
+	Data   []byte `json:"data"`
 }
 
 var (
-	peerList	Peers
-	partSize	int64		= 2097152	// 2 Mbytes in bytes
-	partNums	int		= 0
-	partList	Parts
-	sFile		*os.File
-	fileSize	int64		= 0
-	port		string 		= "8080"
+	peerList Peers
+	partSize int64 = 2097152 // 2 Mbytes in bytes
+	partNums int             //= 0
+	partList Parts
+	sFile    *os.File
+	fileSize int64  //= 0
+	port     string = "8080"
 )
 
 func main() {
 	var (
-		iFile		= flag.String("if", "", "Путь до файла раздачи (для пида)")
-		oFile		= flag.String("of", "", "Путь до файла раздачи (для сира)")
-		targetAdr	= flag.String("t", "", "Адрес для запроса c портом 8080")
+		iFile     = flag.String("if", "", "Путь до файла раздачи (для пида)")
+		oFile     = flag.String("of", "", "Путь до файла раздачи (для сира)")
+		targetAdr = flag.String("t", "", "Адрес для запроса c портом 8080")
 	)
 
 	flag.Parse()
 	peerList = make(Peers)
 	partList = make(Parts)
 
-	fmt.Printf("MAIN: Init file\n")					//DEBUG
+	fmt.Printf("MAIN: Init file\n") //DEBUG
 	if *oFile != "" {
 		tmpFile, err := os.OpenFile(*oFile, os.O_RDONLY, 0755)
 		chkError(err)
@@ -54,16 +57,16 @@ func main() {
 		chkError(err)
 
 		fileSize = tmpLstat.Size()
-		fmt.Printf("Размер файла раздачи- %d байт\n", fileSize)	//DEBUG
+		fmt.Printf("Размер файла раздачи- %d байт\n", fileSize) //DEBUG
 
-		Mapper()						// Обрабатывает данные файла
+		Mapper() // Обрабатывает данные файла
 
-		fmt.Printf("Размер части - %d байт\n", partSize)		//DEBUG
-		fmt.Printf("Кол-во частей - %v\n", partNums)			//DEBUG
-		fmt.Printf("Список частей со смещениями - %d\n", partList)	//DEBUG
+		fmt.Printf("Размер части - %d байт\n", partSize)           //DEBUG
+		fmt.Printf("Кол-во частей - %v\n", partNums)               //DEBUG
+		fmt.Printf("Список частей со смещениями - %d\n", partList) //DEBUG
 
 	} else if *iFile != "" {
-		tmpFile, err := os.OpenFile(*iFile, os.O_RDWR|os.O_CREATE, 0755)	// Создаём файл с нуля
+		tmpFile, err := os.OpenFile(*iFile, os.O_RDWR|os.O_CREATE, 0755) // Создаём файл с нуля
 		chkError(err)
 
 		sFile = tmpFile
@@ -72,19 +75,19 @@ func main() {
 		partList[0] = int64(0)
 	}
 
-	fmt.Printf("Запускам слушателя\n")					//DEBUG
-	ln, err := net.Listen("tcp", ":" + port)
+	fmt.Printf("Запускам слушателя\n") //DEBUG
+	ln, err := net.Listen("tcp", ":"+port)
 	chkError(err)
 	defer ln.Close()
 
 	if *targetAdr != "" {
-		fmt.Printf("Режим пира\n")					//DEBUG
-		fmt.Printf("Коннект с сидом - %s\n\n", *targetAdr)		//DEBUG
+		fmt.Printf("Режим пира\n")                         //DEBUG
+		fmt.Printf("Коннект с сидом - %s\n\n", *targetAdr) //DEBUG
 		go Loader(*targetAdr)
 	} else {
-		fmt.Printf("Режим сида\n\n")					//DEBUG
-		peerList[GetLocalIp()] = partNums
-		fmt.Printf("Добавили себя, первого пира - %v\n", peerList)	//DEBUG
+		fmt.Printf("Режим сида\n\n") //DEBUG
+		peerList[GetLocalIP()] = partNums
+		fmt.Printf("Добавили себя, первого пира - %v\n", peerList) //DEBUG
 	}
 
 	for {
@@ -95,11 +98,11 @@ func main() {
 
 		tmpPart := Reader(conn, sFile)
 
-		if _, ok := peerList[s]; ok == false {		// Если нет пира с таким ключом в мапе
+		if _, ok := peerList[s]; ok == false { // Если нет пира с таким ключом в мапе
 
-			fmt.Printf("Добавлен пир: %s\n", s)		//DEBUG
-			peerList[s] = tmpPart				// Добавляем пира
-			
+			fmt.Printf("Добавлен пир: %s\n", s) //DEBUG
+			peerList[s] = tmpPart               // Добавляем пира
+
 			if *targetAdr == "" {
 				var ta Mssg
 				ta.Header = "PUSH"
@@ -108,35 +111,37 @@ func main() {
 				b, err := json.Marshal(ta)
 				chkError(err)
 
-				fmt.Printf("Текущий список пиров, обновляем у пиров: %v\n", peerList)	//DEBUG
+				fmt.Printf("Текущий список пиров, обновляем у пиров: %v\n", peerList) //DEBUG
 				for addr, _ := range peerList {
-					if addr != GetLocalIp() {
-						Dialer(addr + ":" + port, b)
+					if addr != GetLocalIP() {
+						Dialer(addr+":"+port, b)
 					}
 				}
 			}
-			fmt.Printf("\n")					//DEBUG
+			fmt.Printf("\n") //DEBUG
 		}
 	}
 }
 
+// Mapper : Размечает файл на части
 func Mapper() {
 	if fileSize <= partSize {
 		partNums = 0
 		partList[0] = int64(0)
 	} else {
-		tmpStr := strconv.FormatInt(fileSize/ partSize, 10)
+		tmpStr := strconv.FormatInt(fileSize/partSize, 10)
 		tmpNums, err := strconv.Atoi(tmpStr)
 		chkError(err)
 
-		partNums = tmpNums + 1                          // +1 На случай недобора последней части до 2 Мбайт
+		partNums = tmpNums + 1 // +1 На случай недобора последней части до 2 Мбайт
 		for i := 0; i < partNums; i++ {
 			partList[i] = int64(i) * partSize
 		}
 	}
 }
 
-func GetLocalIp() string {                      			//
+// GetLocalIP : Добывает свой локальный адрес по имени хоста
+func GetLocalIP() string {
 	host, _ := os.Hostname()
 	addrs, _ := net.LookupIP(host)
 	for _, addr := range addrs {
@@ -144,18 +149,19 @@ func GetLocalIp() string {                      			//
 			return fmt.Sprintf("%s", ipv4)
 		}
 	}
-	return "localhost"						//!!!!!!!
+	return "localhost" //!!!!!!!
 }
 
+// Dialer : Производит отправку сообщений
 func Dialer(ts string, msg []byte) {
-		conn, err := net.Dial("tcp", ts)
-		chkError(err)
+	conn, err := net.Dial("tcp", ts)
+	chkError(err)
 
-		writedByte, err := conn.Write(msg)
-		chkError(err)
+	writedByte, err := conn.Write(msg)
+	chkError(err)
 
-		fmt.Printf("Байт отправлено: %d\n", writedByte)	//DEBUG
-		conn.Close()
+	fmt.Printf("Байт отправлено: %d\n", writedByte) //DEBUG
+	conn.Close()
 }
 
 func chkError(err error) {
@@ -165,10 +171,11 @@ func chkError(err error) {
 	}
 }
 
+// Reader : Обрабатывает пришедшую инфу из соединения
 func Reader(c net.Conn, f *os.File) int {
 	var (
-		a Mssg		// Входящее
-		ta Mssg		// Исходящее
+		a  Mssg // Входящее
+		ta Mssg // Исходящее
 	)
 
 	s := c.RemoteAddr().String()
@@ -178,99 +185,101 @@ func Reader(c net.Conn, f *os.File) int {
 	readedByte, err := c.Read(b)
 	chkError(err)
 
-	err = json.Unmarshal(b[0:readedByte],&a)
+	err = json.Unmarshal(b[0:readedByte], &a)
 	chkError(err)
 
-	switch(a.Header) {
-		case "INITA":
-			fmt.Printf("Получен запрос на инициализацию\n")			//DEBUG
-			
-			ta.Header = "INITB"
-			ta.Part = 0
-			ta.Data = []byte(fmt.Sprintf("%d:%d", partNums, fileSize))
+	switch a.Header {
+	case "INITA":
+		fmt.Printf("Получен запрос на инициализацию\n") //DEBUG
 
-			tb, err := json.Marshal(ta)
-			chkError(err)
+		ta.Header = "INITB"
+		ta.Part = 0
+		ta.Data = []byte(fmt.Sprintf("%d:%d", partNums, fileSize))
 
-			Dialer(tmpAddr + ":" + port, tb)
-			fmt.Printf("Отправили ответ на инициализацию\n")		//DEBUG
-			
-		case "INITB":
-			fmt.Printf("Пришел ответ на инициализацию: %s\n", s)		//DEBUG
+		tb, err := json.Marshal(ta)
+		chkError(err)
 
-			pInfo := strings.Split(string(a.Data), ":")
-			tmpNums, err := strconv.Atoi(pInfo[0])
-			tmpSize, err := strconv.ParseInt(pInfo[1] ,10,64)
-			chkError(err)
+		Dialer(tmpAddr+":"+port, tb)
+		fmt.Printf("Отправили ответ на инициализацию\n") //DEBUG
 
-			partNums = tmpNums
-			fileSize = tmpSize
-			Mapper()
+	case "INITB":
+		fmt.Printf("Пришел ответ на инициализацию: %s\n", s) //DEBUG
 
-			fmt.Printf("Получено кл-во частей: %d\n", partNums)			//DEBUG
-			fmt.Printf("Получен размер файла: %d\n", fileSize)			//DEBUG
-			fmt.Printf("Получен список частей со смещениями - %d\n", partList)	//DEBUG
+		pInfo := strings.Split(string(a.Data), ":")
+		tmpNums, err := strconv.Atoi(pInfo[0])
+		chkError(err)
+		tmpSize, err := strconv.ParseInt(pInfo[1], 10, 64)
+		chkError(err)
 
-//		case "GET":
-//			fmt.Printf("READER: GET part: %s\n", tmpArr[1])			//DEBUG
-//			tmpData := make([]byte, partSize)
-//			
-//			count, err := f.ReadAt(tmpData, partList[tmpPart])
-//			if err != nil {
-//				if err.Error() != "EOF" {
-//					chkError(err)
-//				}
-//			}
-//
-//			data := string(tmpData[0:count])
-//
-//			fmt.Printf("READER: Send TAKE\n")				//DEBUG
-//			Dialer(tmpAddr + ":" + port, "TAKE:" + tmpArr[1] + ":" + data)
+		partNums = tmpNums
+		fileSize = tmpSize
+		Mapper()
 
-//		case "TAKE":
-//
-//			tmpData := []byte(tmpArr[2])
-//			_, err := f.WriteAt(tmpData, partList[tmpPart])
-//			chkError(err)
-//
-//			fmt.Printf("READER: TAKE: %s\n", tmpArr[2])	//DEBUG
+		fmt.Printf("Получено кл-во частей: %d\n", partNums)                //DEBUG
+		fmt.Printf("Получен размер файла: %d\n", fileSize)                 //DEBUG
+		fmt.Printf("Получен список частей со смещениями - %d\n", partList) //DEBUG
 
-		case "PUSH":
-			fmt.Printf("Пришло обновление списка пиров\n")			//DEBUG
-			tmpStr := string(a.Data)
-			tmpStr = strings.Trim(tmpStr, "map[]")
-			tmpArr := strings.Split(tmpStr, " ")
-			for i := 0; i < len(tmpArr); i++ {
-				pInfo := strings.Split(tmpArr[i], ":")
-				if _, ok := peerList[pInfo[0]]; ok == false {
-					tint, err := strconv.Atoi(pInfo[1])
-					chkError(err)					
-					peerList[pInfo[0]] = tint
-				}
+		//		case "GET":
+		//			fmt.Printf("READER: GET part: %s\n", tmpArr[1])			//DEBUG
+		//			tmpData := make([]byte, partSize)
+		//
+		//			count, err := f.ReadAt(tmpData, partList[tmpPart])
+		//			if err != nil {
+		//				if err.Error() != "EOF" {
+		//					chkError(err)
+		//				}
+		//			}
+		//
+		//			data := string(tmpData[0:count])
+		//
+		//			fmt.Printf("READER: Send TAKE\n")				//DEBUG
+		//			Dialer(tmpAddr + ":" + port, "TAKE:" + tmpArr[1] + ":" + data)
+
+		//		case "TAKE":
+		//
+		//			tmpData := []byte(tmpArr[2])
+		//			_, err := f.WriteAt(tmpData, partList[tmpPart])
+		//			chkError(err)
+		//
+		//			fmt.Printf("READER: TAKE: %s\n", tmpArr[2])	//DEBUG
+
+	case "PUSH":
+		fmt.Printf("Пришло обновление списка пиров\n") //DEBUG
+		tmpStr := string(a.Data)
+		tmpStr = strings.Trim(tmpStr, "map[]")
+		tmpArr := strings.Split(tmpStr, " ")
+		for i := 0; i < len(tmpArr); i++ {
+			pInfo := strings.Split(tmpArr[i], ":")
+			if _, ok := peerList[pInfo[0]]; ok == false {
+				tint, err := strconv.Atoi(pInfo[1])
+				chkError(err)
+				peerList[pInfo[0]] = tint
 			}
-			fmt.Printf("Список пиров после обновления - %v\n", peerList)		//DEBUG
+		}
+		fmt.Printf("Список пиров после обновления - %v\n", peerList) //DEBUG
 	}
-	fmt.Printf("\n")						//DEBUG
+	fmt.Printf("\n") //DEBUG
 	return a.Part
 }
 
+// Loader : Занимается сборкой файла
 func Loader(ta string) {
 
 	a := Mssg{
-		Header:		"INITA",
-		Part:		0,
-		Data:		[]byte("0"),
+		Header: "INITA",
+		Part:   0,
+		Data:   []byte("0"),
 	}
 
 	b, err := json.Marshal(a)
 	chkError(err)
 
-	fmt.Printf("Отправлен запрос на инициализацию - %s\n", b)	//DEBUG
+	fmt.Printf("Отправлен запрос на инициализацию - %s\n", b) //DEBUG
 
 	Dialer(ta, b)
-//	time.Sleep(1 * time.Second)
-//	for i, _ := range partList {
-//		Dialer(ta, "GET:" + strconv.Itoa(i))
-//		time.Sleep(1 * time.Second)	
-//	}
+	//	time.Sleep(1 * time.Second)
+	//	for i, _ := range partList {
+	//		Dialer(ta, "GET:" + strconv.Itoa(i))
+	//		time.Sleep(1 * time.Second)
+	//	}
 }
